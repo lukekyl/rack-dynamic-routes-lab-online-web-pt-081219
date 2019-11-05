b6d6a46cbdac272f4f2cf0d9df39505ccf15d481
Kyle Luke <th.oldschool@gmail.com>
2019-11-05 03:32:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,11 +14,11 @@ class Application
         resp.write item.price
       else
         resp.write "Item not found"
-        resp.status 400
+        resp.status = 400
       end
     else
       resp.write "Page not found"
-      resp.status 404
+      resp.status = 404
     end
 
     resp.finish
