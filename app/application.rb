a0069b4d85db4a09e0dfb7bdd057a11be2c9aebf
Kyle Luke <th.oldschool@gmail.com>
2019-11-05 03:25:08 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+ 
+  @@items = []
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/songs/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+    end
+ 
+    resp.finish
+  end
+end
